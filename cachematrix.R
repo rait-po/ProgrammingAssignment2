a05b73d3cc37850a9fdc6c5a7608a333ad171fa9
rait-po <rait.po@gmail.com>
2015-02-22 02:48:03 +0200

    functions for matrix inversion with cache
    solution is based on example that was provided

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+## I need to create vector/list of functions to be used by function "cacheSolve" 
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the solve
+## 4.get the value of the solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cmem <- NULL
+  set <- function(y) {
+    x <<- y
+    cmem <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(solve) cmem <<- solve
+  getsolved <- function() cmem
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved)
 }
 
 
 ## Write a short comment describing this function
+## This will call the functions defined in "makeCacheMatrix" to apply caching functionality
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  cmem <- x$getsolved()
+  if(!is.null(cmem)) {
+    message("getting cached data")
+    return(cmem)
+  }
+  data <- x$get()
+  cmem <- solve(data, ...)
+  x$setsolved(cmem)
+  cmem
 }
